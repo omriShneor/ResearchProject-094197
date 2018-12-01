c98eb562e0b639b0714cc0dfbc2ac1da3f931a02
RND\omrish <omrishneor@gmail.com>
2018-12-01 18:39:29 +0200

    Major changes and improvments in the growth curve project. Advanced a lot and now the plots show the expected cureves aswell, still need some adjustments and fixing but overall it looks good.

--- GrowthCurves/growth_curves.py
+++ GrowthCurves/growth_curves.py
@@ -49,9 +49,10 @@ def add_delta_from_start_in_hours(df_per_id):
         df['Date'] = pd.to_datetime(df['Date'])
         df['Hours_From_First_Sample'] = (df['Date'] - df['Date'].iloc[0])
         df['Hours_From_First_Sample'] = df['Hours_From_First_Sample']/np.timedelta64(1,'h')
+        df['Weight'] = df['Weight']*1000
     return df_per_id
 
-def plot_data_per_id(df_per_id,plot_kind):
+def plot_data_per_id(df_per_id,expected_weight_df):
     '''
     adding a column of 'Hours from first sample` to each dataframe describing the
     time difference between each test and the first test taken for that ID.
@@ -60,16 +61,44 @@ def plot_data_per_id(df_per_id,plot_kind):
     the described column above added to it
     '''
     for i in range(len(df_per_id)):
-        df_per_id[i].plot(kind=plot_kind,x='Hours_From_First_Sample',y='Weight',color='red',
-                yticks=np.arange(0, 6, 0.2),ylim = (0,6), title="Plot For Subject No: " + str(df_per_id[i]['ID'].real[0]))
-        directory = ROOT_DIR + "\\Plots\\"
-        plt.savefig(directory+'plt-result-' + str(df_per_id[i]['ID'].real[0]) + '.png')
+        idx = filter_suitable_expected_weight(df_per_id[i], weight_at_birth_series, expected_weight_df)
+        suitable_df = expected_weight_df.iloc[:,[0,idx-1]]
+        suitable_df.iloc[:,0] = suitable_df.iloc[:,0]*24
+        suitable_df = suitable_df.dropna()
+        suitable_df.columns = range(suitable_df.shape[1])
+        suitable_df.rename(columns={0:'Hours_From_First_Sample',1:'ExpectedWeight'},inplace=True)
+        ax = suitable_df.plot(kind='line',x='Hours_From_First_Sample',y='ExpectedWeight',color='blue')
+        df_per_id[i].plot(kind='scatter',x='Hours_From_First_Sample',y='Weight',color='red',
+                yticks=np.arange(0, 6000, 200),ylim = (0,6000), title="Plot For Subject No: "+
+                                                                str(df_per_id[i]['ID'].real[0]),ax=ax)
+        plt.show()
+        # directory = ROOT_DIR + "\\Plots\\"
+        # plt.savefig(directory+'plt-result-' + str(df_per_id[i]['ID'].real[0]) + '.png')
         plt.clf()
     return
+
+def collect_expected_weight():
+    # collect the dataset and remove the headers from it.
+    datasets_dir = ROOT_DIR + '\\DataSet'
+    df = pd.read_excel(datasets_dir + '\\' + "ExpectedWeight.xlsx", header=None,skiprows=1)
+    return df
+
+def collect_weight_per_date_of_birth(df):
+    # Remove the column for Days old e.g. the zero redundent value
+    return df.iloc[0][1::]
+
+def filter_suitable_expected_weight(df_per_id,weight_at_birth_series,expected_weight_df):
+    weight_at_birth_in_grams = df_per_id.iloc[0]["Weight"] * 1000
+    idx = weight_at_birth_series.searchsorted(weight_at_birth_in_grams, side='left')
+    return idx
 
 
 logging.basicConfig(filename="GrowthCurves.log", level=logging.INFO)
 cleaned_df = extract_clean_wieght_data()
+expected_weight_df = collect_expected_weight()
+weight_at_birth_series = collect_weight_per_date_of_birth(expected_weight_df)
 df_per_id = extract_dataframe_per_id(cleaned_df)
 df_per_id = add_delta_from_start_in_hours(df_per_id)
-plot_data_per_id(df_per_id,'scatter')
+plot_data_per_id(df_per_id,expected_weight_df)
+
+print("sasi")
